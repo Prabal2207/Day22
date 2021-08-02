69d5d89a52438e9121f9b0c7890151a66132ef5c
Prabal2207 <prabal760@gmail.com>
2021-08-02 09:40:18 +0530

    [Prabal] Added:-Annotation validation for User Registration

--- DAY22_Annotation/Program.cs
+++ DAY22_Annotation/Program.cs
@@ -8,47 +8,47 @@ namespace DAY22_Annotation
     {
         static void Main(string[] args)
         {
-            //UserRegistraionAnnotation obj = new UserRegistraionAnnotation();
+            UserRegistraionAnnotation obj = new UserRegistraionAnnotation();
             Console.WriteLine("Welcome to Exception Handling Implementation!");
 
-            //string FirstName, LastName, email, PhoneNumber, Password;
+            string FirstName, LastName, email, PhoneNumber, Password;
 
-            //Console.Write("Enter First Name: ");
-            //FirstName = Console.ReadLine();
+            Console.Write("Enter First Name: ");
+            FirstName = Console.ReadLine();
 
-            //obj.FirstName = FirstName;
-
-
-            //Console.Write("Enter Last Name: ");
-            //LastName = Console.ReadLine();
-            //obj.LastName = LastName;
+            obj.FirstName = FirstName;
 
 
-            //Console.Write("Enter your email addresss: ");
-            //email = Console.ReadLine();
-            //obj.Email_Id = email;
+            Console.Write("Enter Last Name: ");
+            LastName = Console.ReadLine();
+            obj.LastName = LastName;
 
 
-            //Console.Write("Enter your  Phone number: ");
-            //PhoneNumber = Console.ReadLine();
-            //obj.PhoneNumber = PhoneNumber;
-
-            //Console.Write("Enter your  Password: ");
-            //Password = Console.ReadLine();
-            //obj.Password = Password;
+            Console.Write("Enter your email addresss: ");
+            email = Console.ReadLine();
+            obj.Email_Id = email;
 
 
-            //ValidationContext context = new ValidationContext(obj, null, null);
-            //List<ValidationResult> results = new List<ValidationResult>();
-            //bool valid = Validator.TryValidateObject(obj, context, results, true);
+            Console.Write("Enter your  Phone number: ");
+            PhoneNumber = Console.ReadLine();
+            obj.PhoneNumber = PhoneNumber;
 
-            //if (!valid)
-            //{
-            //    foreach (ValidationResult TotalResult in results)
-            //    {
-            //        Console.WriteLine("Error Msg : {0}{1}", TotalResult.ErrorMessage, Environment.NewLine);
-            //    }
-            //}
+            Console.Write("Enter your  Password: ");
+            Password = Console.ReadLine();
+            obj.Password = Password;
+
+
+            ValidationContext context = new ValidationContext(obj, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(obj, context, results, true);
+
+            if (!valid)
+            {
+                foreach (ValidationResult TotalResult in results)
+                {
+                    Console.WriteLine("Error Msg : {0}{1}", TotalResult.ErrorMessage, Environment.NewLine);
+                }
+            }
 
             Console.ReadKey();
 
---
+++ DAY22_Annotation/UserRegistraionAnnotation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAY22_Annotation
+{
+    public  class UserRegistraionAnnotation
+    {
+
+        //UC1
+        [Required(ErrorMessage = "User {0} is required")]
+        [RegularExpression("^[A-Z][A-Za-z]{2,}$", ErrorMessage = "Invalid input minimum 1 upper case and 2 chars are required")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+
+
+        
+        //UC2
+        [Required(ErrorMessage = "User {0} is required")]
+        [RegularExpression("^[A-Z][A-Za-z]{2,}$", ErrorMessage = "Invalid Input minimum 1 upper case and 2 chars are required")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        
+
+        //UC3
+        [Required(ErrorMessage = "User {0} is required")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email_Id { get; set; }
+       
+
+        //UC4
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+       
+
+        //UC5-8
+        [Required(ErrorMessage = "User {0} is required")]
+        [RegularExpression("((?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%]).[A-Z0-9a-z@#$%]{8,})", ErrorMessage = "Invalid input minimum 1 upper case, one special char, one nummeric value and 8 chars are required")]
+        [DataType(DataType.Text)]
+        public string Password { get; set; }
+        
+    }
+}
+
